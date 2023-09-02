2577fe3af31ed896f9ca283ecfc5755b3c152eb6
Ashraf Shah <34867871+aashah15@users.noreply.github.com>
2023-09-02 03:00:44 +0000

    more functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,5 +47,44 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Ashraf"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage("Ashraf"))
+// console.log(loginUserMessage())
+
+// function calculateCartPrice(num1){
+//     return num1
+// }
+
+// function calculateCartPrice(...num1){ // using spread / rest operator any number of values, this will return an array of values [200, 400, 500, 2000]
+//     return num1
+// }
+
+function calculateCartPrice(val1, val2, ...num1){ // first two values will go to val1 and val2 and the rest will go to an array
+    return num1
+}
+
+// console.log(calculateCartPrice(200))
+// console.log(calculateCartPrice(200, 400, 500, 2000)) 
+
+const user = {
+    username: "Ashraf",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
